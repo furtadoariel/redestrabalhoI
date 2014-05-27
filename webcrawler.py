055285ffdf3f1acc9292f762626b7cf5922669b6
Ariel Furtado <afazevedo@inf.ufpel.edu.br>
2014-05-27 17:06:23 -0300

    recursao

--- webcrawler.py
+++ webcrawler.py
@@ -4,27 +4,38 @@ from bs4 import BeautifulSoup
 import requests
 
 def webcrawler (deep, start_url):
+	deep = deep
 	lista = []
 	listaimg = []
 	i = 0
 	j = 0
 	r = requests.get(start_url)
 	soup = BeautifulSoup(r.text)
-	for a in soup.findAll('a',href=True):
-		link = a['href']
-		i += 1
-		print link
-		lista.append(link)
-		#links = str(link).strip('[]').replace("http://", "").replace("u", "")
-		link = link.replace("http://", "")
-		links = str(link)
-		print str(i) + ") " + str(links)
-	for img in soup.findAll('img', src=True):
-		imglink = img['src']
-		j += 1
-		print imglink
-		listaimg.append(imglink)
-		
+	for d in xrange(deep):
+		print d
+		if d == 0:
+			
+			for a in soup.findAll('a',href=True):
+				link = a['href']
+				i += 1
+				print link
+				lista.append(link)
+				#links = str(link).strip('[]').replace("http://", "").replace("u", "")
+				link = link.replace("http://", "")
+				links = str(link)
+				print str(i) + ") " + str(links)
+			for img in soup.findAll('img', src=True):
+				imglink = img['src']
+				j += 1
+				print imglink
+				listaimg.append(imglink)
+		else:
+			for lenght in xrange(len(lista)):
+				#print lista[lenght]
+				webcrawler(d, lista[lenght] )
+				
+				
+			
 	print "\n>> Total = %d \n" % i + "\n>> Total Imagens = %d \n" % j
 	sys.exit()
 
@@ -33,4 +44,4 @@ def webcrawler (deep, start_url):
 			
 
 url = raw_input(">> ")
-webcrawler(10, url)
+webcrawler(2, url)
